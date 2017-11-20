1e7db90f870d22f120467aa0f015a8641eed34d6
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-20 11:49:35 -0500

    add edit action, update methods, and links to edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
-      @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(article_params)
@@ -22,8 +25,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
-  
+
   def article_params
     params.require(:article).permit(:title, :text)
   end
