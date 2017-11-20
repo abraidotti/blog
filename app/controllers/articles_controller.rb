f6cea27bd3294f41bdd0293844c7d8b452601312
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-20 11:36:00 -0500

    add validation to new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
 
-def create
-  @article = Article.new(article_params)
+  def create
+    @article = Article.new(article_params)
 
-  @article.save
-  redirect_to @article
-end
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
-private
+  private
+  
   def article_params
     params.require(:article).permit(:title, :text)
   end
