06ded5bb2af8ff2a5b8af13af51183e6baf59347
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-21 14:36:40 -0500

    fix route on log-in, modify style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
-  def index
-  end
-end
+  before_action :redirect_logged_in, only: [:index]
+
+   def index
+   end
+
+   def redirect_logged_in
+     if current_user
+       redirect_to articles_path
+     end
+   end
+
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :controllers => { registrations: 'registrations' }
 
   devise_for :users
-  # get 'articles/index'
 
   resources :articles do
     resources :comments
