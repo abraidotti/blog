59991128de892a910a8de396b68bc28486b103c3
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-20 20:32:41 -0500

    add more changes to article, user, comment relationships

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,26 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    current_user.comments << @comment
+    current_user.save
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article)
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to article_path(@article)
+    else
+      redirect_to article_path(@article), notice: "bad commenter id"
+    end
   end
 
   private
 
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body)
     end
 
 end
